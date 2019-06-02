59417d8fde5efa5afe357524be032ef01da611d0
Matthew Davis <matthew@matthewdavis.io>
2019-06-02 15:34:17 -0500

    bump

---
+++ src/Commands/UpdateCommand.ts
@@ -0,0 +1,64 @@
+import { Command, CommandBase, CommandParser, Event } from '@autobot/common';
+import axios                                          from 'axios';
+import { RichEmbed }                                  from 'discord.js';
+import * as fs                                        from 'fs';
+
+/**
+ * Downloads the latest db.json file from devdocs.io with an !update <language>.
+ */
+@Command
+export class UpdateCommand extends CommandBase {
+
+    public constructor() {
+
+        //
+        // Set this commands configuration.
+        //
+        super({
+
+            event: Event.MESSAGE,
+            name: '!update',
+            group: 'docs',
+            roles: [
+                'docsbot'
+            ],
+            description: 'Downloads the latest db.json file from devdocs.io with an !update <language>'
+
+        });
+
+    }
+
+    /**
+     * Called when a command matches config.name.
+     *
+     * @param command Parsed out commamd
+     *
+     */
+    public async run(command: CommandParser) {
+
+        const result = await axios(`https://docs.devdocs.io/${ command.arguments[ 0 ].name }/db.json`);
+
+        console.log(result);
+
+        if (result) {
+
+            const writer = fs.createWriteStream(`${ process.env.DOCSBOT_SAVE_PATH }/${ command.arguments[ 0 ].name }.json`);
+
+            console.log(writer);
+
+            command.obj.channel.send(new RichEmbed().setTitle('devdocs update')
+                                                    .setColor(3447003)
+                                                    .setDescription(`Downloaded"${ command.arguments[ 0 ].name }db.json!`));
+
+        } else {
+
+            command.obj.channel.send(new RichEmbed().setTitle('devdocs')
+                                                    .setColor(3447003)
+                                                    .setDescription(`Could not find any results for "${ command.arguments[ 0 ].name }`));
+
+        }
+
+    }
+
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './Commands/JavascriptCommand';
+export * from './Commands/UpdateCommand';
